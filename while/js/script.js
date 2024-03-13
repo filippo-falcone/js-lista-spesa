6e258f73025295d0be88c9a26ebfecc6ce2d18ec
filippo-falcone <falconefilippo98@hotmail.com>
2024-03-13 13:35:33 +0100

    feat: add while cycle folder

---
+++ while/js/script.js
@@ -0,0 +1,13 @@
+/* -----------------------------------------------------------------------------------------------------------
+   Data una lista della spesa:
+   const list = [
+       'Latte',
+       'Cacao',
+       'Insalata',
+       'Carote',
+       'Pomodori',
+       'Piatti pronti'
+   ];
+   Stampare sulla pagina (anche brutalmente, basta che si vedano) gli elementi della lista individualmente.
+   Svolgete questo esercizio in 2 versioni: col ciclo for e col ciclo while.
+---------------------------------------------------------------------------------------------------------- */
