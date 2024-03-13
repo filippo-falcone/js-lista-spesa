b679b476a7381e820651f457f73358544397dc52
filippo-falcone <falconefilippo98@hotmail.com>
2024-03-13 13:28:37 +0100

    feat: add variable & create for cycle

--- for/js/script.js
+++ for/js/script.js
@@ -11,3 +11,21 @@
    Stampare sulla pagina (anche brutalmente, basta che si vedano) gli elementi della lista individualmente.
    Svolgete questo esercizio in 2 versioni: col ciclo for e col ciclo while.
 ---------------------------------------------------------------------------------------------------------- */
+/*
+// Leggo il contenuto dell'array
+// Creo un li
+// Stampo nel DOM l'elemento
+*/
+// Variabili
+const ulContainer = document.querySelector('.lista-spesa');
+const list = [
+    'Latte',
+    'Cacao',
+    'Insalata',
+    'Carote',
+    'Pomodori',
+    'Piatti pronti'
+];
+// Funzionalità
+for (let i = 0; i < list.length; i++) {
+}
