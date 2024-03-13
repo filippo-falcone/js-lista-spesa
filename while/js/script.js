69e2e2466ef32030afc9c01390285b6cddb18632
filippo-falcone <falconefilippo98@hotmail.com>
2024-03-13 13:53:06 +0100

    feat: add while cylce functionality

--- while/js/script.js
+++ while/js/script.js
@@ -11,3 +11,27 @@
    Stampare sulla pagina (anche brutalmente, basta che si vedano) gli elementi della lista individualmente.
    Svolgete questo esercizio in 2 versioni: col ciclo for e col ciclo while.
 ---------------------------------------------------------------------------------------------------------- */
+/*
+// Creo un li
+// Stampo il li sul DOM
+// Creo un ciclo while
+// - Inserisco l'elemento li nel DOM finchè i è minore della lunghezza dell'array
+*/
+// Variabili
+const ulContainer = document.querySelector('#lista-spesa');
+const list = [
+    'Latte',
+    'Cacao',
+    'Insalata',
+    'Carote',
+    'Pomodori',
+    'Piatti pronti'
+];
+// Funzionalità
+let i = 0
+while (i < list.length) {
+    let liElement = document.createElement("li");
+    liElement.innerHTML = list[i];
+    ulContainer.append(liElement);
+    i++;
+}
